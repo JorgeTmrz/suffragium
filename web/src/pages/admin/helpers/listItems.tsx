ad4be3fef075adbfe8d12ea594a722edff2e67d5
Jorge <jorge.tamariz@aloe.com.do>
2021-08-18 19:47:04 -0400

    Change name if principal screen

--- web/src/pages/admin/helpers/listItems.tsx
+++ web/src/pages/admin/helpers/listItems.tsx
@@ -11,7 +11,7 @@ export const mainListItems = (
       <ListItemIcon>
         <MeetingRoom />
       </ListItemIcon>
-      <ListItemText primary="Principal" />
+      <ListItemText primary="Salas" />
     </ListItem>
     <ListItem button>
       <ListItemIcon>
