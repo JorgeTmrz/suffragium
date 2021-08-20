dae356e0ed576eafc3fa93c5fafff095b786467d
Jorge <jorge.tamariz@aloe.com.do>
2021-08-19 20:48:50 -0400

    Add rooms page to router

--- web/src/routes/AdminRoutes.tsx
+++ web/src/routes/AdminRoutes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { Rooms } from "../pages/admin/Rooms";
 import { Settings } from "../pages/admin/Settings";
 import { Users } from "../pages/admin/Users";
 
@@ -8,7 +9,7 @@ export const AdminRoutes = () => {
         <Switch>
             <Route exact path="/ajustes" component={Settings} />
             <Route exact path="/usuarios" component={Users} />
-            <Route path="/" component={() => <div>Hello world</div>} />
+            <Route path="/" component={Rooms} />
         </Switch>
     );
 };
