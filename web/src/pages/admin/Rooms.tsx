1a5562952bdb106f98f13108a5df967eef490175
Jorge <jorge.tamariz@aloe.com.do>
2021-08-19 20:48:13 -0400

    Create rooms page

---
+++ web/src/pages/admin/Rooms.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container} from "@material-ui/core";
+import { RightActionButton } from "../../components/RightActionButton";
+
+export const Rooms = () => {
+
+    return (
+        <Container>
+            <RightActionButton buttonText="Nueva Sala" onClick={() => {}} />
+        </Container>
+    );
+};
