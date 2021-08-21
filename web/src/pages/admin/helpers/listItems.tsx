5ad43e5c895f505b7b8a1aa0a3c242bceaaa6266
Jorge <jorge.tamariz@aloe.com.do>
2021-08-20 22:03:01 -0400

    use history to change pages on left links

--- web/src/pages/admin/helpers/listItems.tsx
+++ web/src/pages/admin/helpers/listItems.tsx
@@ -4,45 +4,47 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import PeopleIcon from "@material-ui/icons/People";
 import { MeetingRoom, Settings } from "@material-ui/icons";
-import { NavLink } from "react-router-dom";
-import { makeStyles } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 export const MainListItems = () => {
-    const linkStyles = makeStyles({
-        appLink: {
-            color: "inherit",
-            textDecoration: "inherit",
-        },
-    });
-
-    const classes = linkStyles();
-
+    const history = useHistory();
     return (
         <>
-            <NavLink className={classes.appLink} to="/">
-                <ListItem button>
-                    <ListItemIcon>
-                        <MeetingRoom />
-                    </ListItemIcon>
-                    <ListItemText primary="Salas" />
-                </ListItem>
-            </NavLink>
-            <NavLink className={classes.appLink} to="usuarios">
-                <ListItem button>
-                    <ListItemIcon>
-                        <PeopleIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Usuarios" />
-                </ListItem>
-            </NavLink>
-            <NavLink className={classes.appLink} to="ajustes">
-                <ListItem button>
-                    <ListItemIcon>
-                        <Settings />
-                    </ListItemIcon>
-                    <ListItemText primary="Ajustes" />
-                </ListItem>
-            </NavLink>
+            <ListItem
+                button
+                onClick={() => {
+                    history.push("/");
+                }}
+            >
+                <ListItemIcon>
+                    <MeetingRoom />
+                </ListItemIcon>
+                <ListItemText primary="Salas" />
+            </ListItem>
+
+            <ListItem
+                button
+                onClick={() => {
+                    history.push("usuarios");
+                }}
+            >
+                <ListItemIcon>
+                    <PeopleIcon />
+                </ListItemIcon>
+                <ListItemText primary="Usuarios" />
+            </ListItem>
+
+            <ListItem
+                button
+                onClick={() => {
+                    history.push("ajustes");
+                }}
+            >
+                <ListItemIcon>
+                    <Settings />
+                </ListItemIcon>
+                <ListItemText primary="Ajustes" />
+            </ListItem>
         </>
     );
 };
