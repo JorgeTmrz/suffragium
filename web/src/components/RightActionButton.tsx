7b76088f8abd7803403a32af6682aecf8800497d
Jorge <jorge.tamariz@aloe.com.do>
2021-08-24 11:21:21 -0400

    parametrize some elements of right action button

--- web/src/components/RightActionButton.tsx
+++ web/src/components/RightActionButton.tsx
@@ -1,15 +1,20 @@
-import { Button, Grid, makeStyles } from "@material-ui/core";
+import { Button, Grid, makeStyles, SvgIconTypeMap } from "@material-ui/core";
 import React from "react";
 import { Add } from "@material-ui/icons";
+import { OverridableComponent } from "@material-ui/core/OverridableComponent";
 
 type rightActionButtonProps = {
     onClick: () => void;
     buttonText: string;
+    color?: "primary" | "inherit" | "default" | "secondary" | undefined;
+    icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">>
 };
 
 export const RightActionButton = ({
     onClick,
     buttonText,
+    icon: Icon = Add,
+    color = "primary"
 }: rightActionButtonProps) => {
     const ButtonPageStyles = makeStyles((theme) => ({
         divider: {
@@ -27,8 +32,8 @@ export const RightActionButton = ({
             justifyContent="flex-end"
             alignItems="flex-end"
         >
-            <Button onClick={onClick} variant="contained" color="primary">
-                <Add />
+            <Button onClick={onClick} variant="contained" color={color}>
+                <Icon />
                 {`${buttonText}`}
             </Button>
         </Grid>
